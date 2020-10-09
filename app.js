77a3f2612092b79d1c4dd509384607515fc006ba
samearth <2018219@iiitdmj.ac.in>
2020-10-09 17:25:21 +0530

    second

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const exp = require('express');
 const app = exp();
 const boutes = require('./routes/boutes');
 
-
-app.listen(3000);
-
-app.use(exp.static('public'));
+var port = process.env.PORT || 3000
+app.listen(port);
 
 app.use(boutes);
